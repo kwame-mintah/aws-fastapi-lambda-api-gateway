7d081c7219338038fac4254370016cd629a6a049
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-05-19 20:46:15 +0100

    feat(AWSDynamoDBService): update expression used for updates, new `Message` as part of insert
    `add_data` will now include adding a message column to the database, `update_data` will only update the `Message` as keys cannot be updated, and will not update if the message is the same. `delete_data` will use the query params passed from the endpoint to delete items

--- app/services/dynamodb_service.py
+++ app/services/dynamodb_service.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from typing import List
 
 import boto3
@@ -14,7 +15,7 @@ MAX_GET_SIZE = 100  # Amazon DynamoDB rejects a get batch larger than 100 items.
 
 class AWSDynamoDBService:
     """
-    Example class to return mock / stubbed data.
+    Encapsulates an Amazon DynamoDB table
     """
 
     def __init__(
@@ -28,12 +29,17 @@ class AWSDynamoDBService:
         self.client = dynamodb_client
 
     def add_data(self, data: InsertData):
+        """
+        Add data to a table.
+        :param data:
+        """
         try:
             self.client.put_item(
                 TableName=self.table,
                 Item={
                     "DateReceived": {"S": str(data.DateReceived)},
                     "UUID": {"S": data.UUID},
+                    "Message": {"S": data.Message},
                 },
             )
         except ClientError as exception:
@@ -43,6 +49,10 @@ class AWSDynamoDBService:
             raise
 
     def get_all_items(self) -> List[dict]:
+        """
+        Get items stored within the table.
+        :return:
+        """
         try:
             response = self.client.scan(TableName=self.table)
         except ClientError as exception:
@@ -53,7 +63,12 @@ class AWSDynamoDBService:
         else:
             return response["Items"] if "Items" in response else []
 
-    def update_data(self, data: InsertData):
+    def update_data(self, data: InsertData) -> dict:
+        """
+        Update an existing data in the table.
+        :param data:
+        :return: Updated attributes.
+        """
         try:
             response = self.client.update_item(
                 TableName=self.table,
@@ -61,6 +76,9 @@ class AWSDynamoDBService:
                     "DateReceived": {"S": str(data.DateReceived)},
                     "UUID": {"S": data.UUID},
                 },
+                UpdateExpression="set Message = :r",
+                ExpressionAttributeValues={":r": {"S": str(data.Message)}},
+                ConditionExpression="attribute_exists(Message) OR Message <> :r",
                 ReturnValues="UPDATED_NEW",
             )
         except ClientError as exception:
@@ -71,13 +89,18 @@ class AWSDynamoDBService:
         else:
             return response["Attributes"]
 
-    def delete_data(self, data: InsertData):
+    def delete_data(self, uuid: str, date_received: datetime):
+        """
+        Delete an item from the table.
+        :param uuid:
+        :param date_received:
+        """
         try:
             self.client.delete_item(
                 TableName=self.table,
                 Key={
-                    "DateReceived": {"S": str(data.DateReceived)},
-                    "UUID": {"S": data.UUID},
+                    "DateReceived": {"S": str(date_received)},
+                    "UUID": {"S": uuid},
                 },
             )
         except ClientError as exception:
